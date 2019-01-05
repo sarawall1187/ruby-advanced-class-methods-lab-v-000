5ac66649384e6bd761e5cad1fa448ca58e467ca4
Sara Walker <sarawall1187@gmail.com>
2019-01-05 20:10:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   end
   
   def self.find_or_create_by_name(song_name)
-   self.create_by_name(song_name) || self.find_by_name(song_name)
+   uniqie_song = self.create_by_name(song_name) || self.find_by_name(song_name)
 
   end
   
