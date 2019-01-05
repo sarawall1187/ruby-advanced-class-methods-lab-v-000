b0ea62991fd2b943c490ba838b32d83d9626be75
Sara Walker <sarawall1187@gmail.com>
2019-01-05 18:49:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,12 +17,14 @@ class Song
   end
   
   def self.new_by_name(song_name)
-  
+    song_name = @@name 
+    
+    
   end
   
-  def self.create_by_name(song_name)
-    song_name = @@name
-    @@all << song_name
-  end
+  # def self.create_by_name(song_name)
+  #   song_name = @@name
+  #   @@all << song_name
+  # end
   
 end
